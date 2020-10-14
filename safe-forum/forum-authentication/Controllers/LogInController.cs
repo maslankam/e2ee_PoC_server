684d6914f2a47abb844f8ad6f9c7f202dd823e7c
chalwa <m.maslanka123@gmail.com>
2020-10-14 14:47:22 +0200

    initial

---
+++ safe-forum/forum-authentication/Controllers/LogInController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace forum_authentication.Controllers
+{
+    [ApiController]
+
+    [Route("[controller]")]
+    public class LogInController : ControllerBase
+    {
+        private readonly ILogger<LogInController> _logger;
+
+        public LogInController(ILogger<LogInController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Authenticate()
+        {
+            return Ok;
+        }
+    }
+}
